50f246c456ef463ef7101ffb5781d3354b11ae90
Luiz Henrique Lima <luizhenrlimaa@gmail.com>
2023-06-26 09:26:18 -0300

    Controle de fluxo da aplicacao

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -10,9 +10,36 @@ func main() {
 	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("3 - Sair do programa")
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
 
+	var comando int
+	fmt.Scan(&comando)
+	//	fmt.Println("O endereço da minha variável comando é", &comando)
+	//	fmt.Println("O comando escolhido foi,", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo...")
+	// } else {
+	// 	fmt.Printf("Digite um comando que seja válido!")
+	// }
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo...")
+	default:
+		fmt.Printf("Digite um comando que seja válido!")
+	}
+
+	// Para verificar o tipo da váriavel
 	// fmt.Println("O tipo da variavel é", reflect.TypeOf(versao))
 }
