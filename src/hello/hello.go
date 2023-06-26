e573b49e7ab22edfd1a4c47c1d321cd3c6c88b58
Luiz Henrique Lima <luizhenrlimaa@gmail.com>
2023-06-26 09:50:12 -0300

    Fazendo o uso de funcoes

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,22 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	nome := "Luiz"
-	versao := 1.1
-	fmt.Println("Olá, sr.", nome)
-	fmt.Println("Este programa está na versão", versao)
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do programa")
+	exibeIntroducao()
 
-	var comando int
-	fmt.Scan(&comando)
-	//	fmt.Println("O endereço da minha variável comando é", &comando)
-	//	fmt.Println("O comando escolhido foi,", comando)
+	exibeMenu()
+
+	comando := leComando()
 
 	// if comando == 1 {
 	// 	fmt.Println("Monitorando...")
@@ -35,11 +29,35 @@ func main() {
 	case 2:
 		fmt.Println("Exibindo Logs...")
 	case 0:
-		fmt.Println("Saindo...")
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
 	default:
 		fmt.Printf("Digite um comando que seja válido!")
+		//Indica que ocorreu algum erro
+		os.Exit(-1)
 	}
-
 	// Para verificar o tipo da váriavel
 	// fmt.Println("O tipo da variavel é", reflect.TypeOf(versao))
+}
+
+func exibeIntroducao() {
+	nome := "Luiz"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
+	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	//	fmt.Println("O endereço da minha variável comando é", &comando)
+	//	fmt.Println("O comando escolhido foi,", comandoLido)
+
+	return comandoLido
 }
